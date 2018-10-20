2f173d0f79affb6b0827722fb556154387bb78c5
Glitch (meadow-swordfish) <none>
2018-10-20 20:36:51 +0000

    🏙🌜 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,92 @@
+$( document ).ready(function() {
+  var items = [];
+  var itemsRaw = [];
+  
+  $.getJSON('/api/books', function(data) {
+    //var items = [];
+    itemsRaw = data;
+    $.each(data, function(i, val) {
+      items.push('<p class="bookItem" id="' + i + '"style="color:'+getRandomColor()+'">' + val.title + ' - ' + val.commentcount + ' comments</p>');
+      return ( i !== 14 );
+    });
+    if (items.length >= 15) {
+      items.push('<p>...and '+ (data.length - 15)+' more!</p>');
+    }
+    $('<p/>', {
+      'class': 'listWrapper',
+      html: items.join('')
+      }).appendTo('#display');
+  });
+  
+  var comments = [];
+  $('#display').on('click','p.bookItem',function() {
+    $("#detailTitle").html('<b>'+itemsRaw[this.id].title+'</b> (id: '+itemsRaw[this.id]._id+')');
+    $.getJSON('/api/books/'+itemsRaw[this.id]._id, function(data) {
+      comments = [];
+      $.each(data.comments, function(i, val) {
+        comments.push('<li>' +val+ '</li>');
+      });
+      comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
+      comments.push('<br><button class="btn btn-info addComment" id="'+ data._id+'">Add Comment</button>');
+      comments.push('<button class="btn btn-danger deleteBook" id="'+ data._id+'">Delete Book</button>');
+      $('#detailComments').html(comments.join(''));
+    });
+  });
+  
+  $('#bookDetail').on('click','button.deleteBook',function() {
+    $.ajax({
+      url: '/api/books/'+this.id,
+      type: 'delete',
+      success: function(data) {
+        //update list
+        $('#detailComments').html('<p style="color: red;">'+data+'<p><p>Refresh the page</p>');
+      }
+    });
+  });  
+  
+  $('#bookDetail').on('click','button.addComment',function() {
+    var newComment = $('#commentToAdd').val();
+    $.ajax({
+      url: '/api/books/'+this.id,
+      type: 'post',
+      dataType: 'json',
+      data: $('#newCommentForm').serialize(),
+      success: function(data) {
+        comments.unshift(newComment); //adds new comment to top of list
+        $('#detailComments').html(comments.join(''));
+      }
+    });
+  });
+  
+  $('#newBook').click(function() {
+    $.ajax({
+      url: '/api/books',
+      type: 'post',
+      dataType: 'json',
+      data: $('#newBookForm').serialize(),
+      success: function(data) {
+        //update list
+      }
+    });
+  });
+  
+  $('#deleteAllBooks').click(function() {
+    $.ajax({
+      url: '/api/books',
+      type: 'delete',
+      dataType: 'json',
+      data: $('#newBookForm').serialize(),
+      success: function(data) {
+        //update list
+      }
+    });
+  }); 
+  function getRandomColor(){
+ let letters = '0123456789';
+ let color = '#';
+  for(let i=0;i<6;i++){
+    color+= letters[Math.floor(Math.random()*10)];
+  }
+  return color;
+}
+});
